0eb4094f51cdc2ab449e02da916f505a4645017c
P50125111 <John.Auld@capita.com>
2024-04-25 14:59:44 +0100

    Removed throws clause from step definition

--- src/test/java/glue/AccountSteps.java
+++ src/test/java/glue/AccountSteps.java
@@ -9,7 +9,7 @@ public class AccountSteps {
     Account account = null;
 
     @Given("^Account exists for Acc No\\. \"([^\"]*)\" with Name \"([^\"]*)\"$")
-    public void accountExistsForAccNoWithName(String number, String name) throws Throwable {
+    public void accountExistsForAccNoWithName(String number, String name) {
         throw new PendingException();
     }
 
